1b0098fc9e8f5f636dca4ff0e2f001485d10f022
Laxsan24 <p.laxsan-2004@hotmail.com>
2025-11-18 18:43:20 +0000

    Static file middleware working and functions  working

---
+++ src/server.js
@@ -0,0 +1,107 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import path from "path";
+import fs from "fs";
+import morgan from "morgan";
+import { fileURLToPath } from "url";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+dotenv.config();
+
+
+const app = express();
+//Express
+app.use(express.json());
+app.use(morgan("short"));
+const PORT = process.env.PORT || 3000;
+
+const uri = process.env.MONGO_URI; 
+
+if (!uri) {
+  console.error("❌ ERROR: MONGO_URI is undefined. Check your .env file!");
+  process.exit(1);
+}
+
+const client = new MongoClient(uri);
+let db;
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    db = client.db("mydatabase");
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+  }
+}
+
+connectToDatabase();
+
+//Logger middleware showing data, URL and method.
+app.use(function(req,res,next){
+  const minutes = (new Date()).getTimezoneOffset();
+  if ((minutes % 2) === 0) {
+    next();
+  }
+  else {
+    res.statusCode = 404;
+    res.end("Not authorized");
+  }
+  });
+app.use(function(req,res,next){
+  console.log("Request date", + new Date());
+  console.log("Request URL:" + req.url);
+  console.log("Request Method:" + req.method);
+  next();
+});
+
+//Static file middleware
+
+app.use(function(req,res,next){
+  const filePath = path.join(_dirname, "static", req.url);
+  fs.stat(filePath, function(err, fileInfo) {
+    if (err) {
+        next();
+        return;
+    }
+    if (fileInfo.isFile()) {
+      res.sendFile(filePath);
+    } else {
+        next();
+    }
+   });
+});
+
+const staticPath = path.join(_dirname, "static");
+app.use(express.static(staticPath));
+
+
+app.get("/", (req, res)=>{
+  res.send("Hello World");
+});
+app.get("/lessons", function (req, res){
+  res.send("Lessons page");
+});
+app.get("/lessons/:id", function(req, res) {
+  const id = parseInt(req.params.id, 10);
+  res.send("Product ID: " + id);
+});
+
+app.get("/search", function (req, res) {
+  res.send("Search functionality");
+});
+app.post("/orders", function (req, res){
+  res.send("Order confirmed");
+});
+app.put("/orders", function(req,res){
+  res.send("Order")
+});
+
+app.use(function(req, res) {
+  res.status(404).send("Sorry, that route doesn't exist.");
+});
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
